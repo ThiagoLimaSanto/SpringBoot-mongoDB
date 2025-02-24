fddbf151ca20047c51ab110b9d4ae123cd08d4a6
Thiago Lima Santos <tlima7601@gmail.com>
2025-02-24 16:04:17 -0300

    getting a user by id, and exception handling

--- src/main/java/com/application/workshop_mongodb/services/UserService.java
+++ src/main/java/com/application/workshop_mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.application.workshop_mongodb.domain.User;
 import com.application.workshop_mongodb.repository.UserRepository;
+import com.application.workshop_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,7 +15,15 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found!"));
+        if (user == null) {
+            throw new ObjectNotFoundException("User not found!");
+        }
+        return user;
     }
 }
