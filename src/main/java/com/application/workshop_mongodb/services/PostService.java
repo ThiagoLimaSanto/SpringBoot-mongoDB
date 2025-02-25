7ce4c3aa98ec94e284407dd75fe830c71bbabc99
Thiago Lima Santos <tlima7601@gmail.com>
2025-02-25 15:50:48 -0300

    custom query

--- src/main/java/com/application/workshop_mongodb/services/PostService.java
+++ src/main/java/com/application/workshop_mongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.application.workshop_mongodb.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 1120);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
